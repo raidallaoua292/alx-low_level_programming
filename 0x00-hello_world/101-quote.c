d2c5104a52d5e1d876c70f642130a80ac963dad5
raidallaoua292 <71762712+raidallaoua292@users.noreply.github.com>
2023-03-16 23:18:59 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,13 +5,13 @@
  *
  * Description: A C program that prints with write
  *
- * Return: Always 0 (Success)
+ * Return: Always 1 (not Success)
  */
 
 int main(void)
 {
 	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(1, qou, 59);
+	write(1, quo, 59);
 	return (1);
 }
