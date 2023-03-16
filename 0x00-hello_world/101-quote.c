d7b63c9f556282f454a8b0cf4ed6c303498c8a3d
raidallaoua292 <raidallaoua292@gmail.com>
2023-03-16 14:05:29 -0700

    update

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with write
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, qou, 59);
+	return (1);
+}
