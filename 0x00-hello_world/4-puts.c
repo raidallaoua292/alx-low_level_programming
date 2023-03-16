873e96d6f25548eac93564ad8857b4ddf4a335c4
raidallaoua292 <71762712+raidallaoua292@users.noreply.github.com>
2023-03-16 20:41:22 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - Entry Point 
+ * main - Entry Point
  *
  * Descriptio : A C program taht prints wrth put function
  *
@@ -9,6 +9,6 @@
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0);
 }
