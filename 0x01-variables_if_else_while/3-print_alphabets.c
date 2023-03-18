a9f83e3b55b9868a4e827caab8801d6001228dc6
raidallaoua292 <raidallaoua292@gmail.com>
2023-03-18 11:10:57 -0700

    upload_file

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Description: this programe print alphabet lowercases
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int str;
+
+	for (str = 'a'; str <= 'z'; str++)
+	{
+		putchar(str);
+	}
+
+	for (str = 'A'; str <= 'Z'; str++)
+	{
+		putchar(str);
+	}
+
+	putchar('\n');
+	return (0);
+}
