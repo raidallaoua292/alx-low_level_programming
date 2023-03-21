e0945a0394010e94aceb07c3491e67eaeb3a5953
raidallaoua292 <raidallaoua292@gmail.com>
2023-03-21 04:11:52 -0700

    upload_files

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Description: this programe prints _putchar
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
