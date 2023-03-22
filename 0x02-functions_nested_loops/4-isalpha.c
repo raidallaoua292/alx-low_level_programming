468be8ed1f4bf715546274e115e6c31580763d26
raidallaoua292 <raidallaoua292@gmail.com>
2023-03-22 15:22:30 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -7,10 +7,15 @@
  */
 int _islower(int c)
 {
-	if (c > 'a' && c < 'z' || c > 'A' && c < 'Z')
+	if (c > 'a' && c < 'z')
 	{
 		return (1);
 	}
+
+	if (c > 'A' && c < 'Z')
+        {
+                return (1);
+        }
 
 	else
 	{
