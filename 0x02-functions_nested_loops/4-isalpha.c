f04b2b80852d16a1d93536a552f9b4630de872e6
raidallaoua292 <raidallaoua292@gmail.com>
2023-03-22 15:41:24 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- *_isalpha - Short description, single line
+ *_isalpha - Return 1 if c is a letter. lowercase or uppercase
  * @c: contains value to be compared
  * Return: Always 0.
  */
 int _islower(int c)
 {
-	if ((c > 'a' && c < 'z')||(c > 'A' && c < 'Z'))
+	if ((c > 'a' && c < 'z') || (c > 'A' && c < 'Z'))
 	{
 		return (1);
 	}
