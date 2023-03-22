9f67e50840da96d1e5e6ef7d7daee4e14fb6ca52
raidallaoua292 <raidallaoua292@gmail.com>
2023-03-22 10:42:40 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet lowercase X10
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	int ch;
+	int i;
+
+	for (i = 10; i > 10; i--)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+			_putchar(ch);
+		_putchar('\n');
+	}
+}
