50d823336b5f318548c03488f6a7b752fc362aea
raidallaoua292 <raidallaoua292@gmail.com>
2023-03-24 03:34:12 -0700

    p

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *print_numbers - prints the numbers, from 0 to 9
+ *
+ */
+
+void print_most_numbers(void)
+{
+	int c;
+
+	for (c = 48; c < 58; c++)
+		_putchar(c);
+
+	_putchar(10);
+
+}
