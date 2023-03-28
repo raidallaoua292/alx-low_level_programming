30eeefa4ff88722593e0e0be9ab9315a2c33b200
raidallaoua292 <raidallaoua292@gmail.com>
2023-03-28 09:45:12 -0700

    upload files

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Description: this programe print alphabet lowercases
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+
+	putchar(num);
+
+	}
+	putchar('\n');
+	return (0);
+}
