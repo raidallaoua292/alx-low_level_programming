4e6997d8ddf743d263234f1f56948727f0c3d6eb
raidallaoua292 <raidallaoua292@gmail.com>
2023-04-10 14:13:55 -0700

    a

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,26 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
+
 /**
- * main - Entry point
+ * main - entry point of the program
+ * @argc: the number of arguments passed to the program
+ * @argv: an array of strings containing the arguments
  *
- * @argc: Counts the number of parameters that go into main
- * @argv: Pointer of array of pointers containing strings entering main
- * Return: Always 0 (Success)
+ * Return: 0 if successful, 1 if an error occurred
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
 	int sum = 0;
-	char *c;
+	int i;
 
-	while (--argc)
+	if (argc == 1)
 	{
-		for (c = argv[argc]; *c; c++)
+		printf("0\n");
+
+		return (0);
+
+	}
+	for (i = 1; i < argc; i++)
+	{
+
+		char *arg = argv[i];
+
+		while (*arg)
+	{
+		if (*arg < '0' || *arg > '9')
 		{
-			if (*c < '0' || * > '9')
-				return (printf("Error\n",1);
+			printf("Error\n");
+			return (1);
 		}
-		sum += atoi(arvg[argc]);
+		arg++;
+	}
+		int num = atoi(argv[i]);
+
+		if (num > 0)
+
+		{
+
+			sum += num;
+		}
 	}
 	printf("%d\n", sum);
 	return (0);
